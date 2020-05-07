40475427e72c008a0f29dfdf89d8c8a2cc09fa3c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-07 17:45:04 -0500

    Add log in form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,27 @@ class UsersController < ApplicationController
 
     if @user.save
       current_user_set(@user.id)
-      redirect_to @user
+      redirect_to current_user
     else
       render :new
+    end
+  end
+
+  def sign_in
+    @user = User.new
+  end
+
+  def logging
+    @error = nil
+    @user = User.find_by_username(params[:user][:username])
+
+    if @user.nil? == false
+      current_user_set(@user.id)
+      redirect_to current_user
+    else
+      @error = "Could not find the user."
+      @user = User.new( { username: params[:user][:username] } )
+      render 'users/sign_in'
     end
   end
 
