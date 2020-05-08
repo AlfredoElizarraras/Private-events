65a8f0ab59833779664b228ea36ea915d5bd36e5
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-08 13:22:27 -0500

    Add some format to the date display in view.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
     def user_created_events
         html_to_render = '<ul>'
         current_user.events.each do |event|
-            p html_to_render += "<li> #{event.date} </li>"
+            p html_to_render += "<li> #{event.date.strftime("Event day on: %m-%d-%Y at: %H:%M")} </li>"
         end
         html_to_render += '</ul>'
         html_to_render.html_safe
