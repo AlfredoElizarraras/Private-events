775b2bf1419d76d61146133e2903db5343a9936d
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-08 13:09:17 -0500

    Fix routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#sign_in'
+  get 'users/sign_in'
   post 'users/logging'
-  root 'users#new'
   resources :users, only:  [:show, :new, :create, :sign_in, :logging]
   resources :events, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
