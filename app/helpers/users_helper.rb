c78b73c398bf92e268c602b9f5a1f735d5666ece
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-08 16:07:48 -0500

    Add some styling.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,17 +4,13 @@ module UsersHelper
     end
 
     def user_created_events
-        html_to_render = '<div class="events-created">'
-        html_to_render += '<h3>'
-        html_to_render += 'MY CREATED EVENTS'
-        html_to_render += '</h3>'
+        html_to_render = ''
         current_user.events.each do |event|
             html_to_render += '<article class="event-card">'
-            html_to_render += "<p> #{event.description} </p>"
+            html_to_render += "<a href=\"\\events\\#{event.id}\"> #{event.description} </a>"
             html_to_render += "<p> #{event.date.strftime("Event day on: %m-%d-%Y at: %H:%M")} </p>"
             html_to_render += '</article>'
         end
-        html_to_render += '</div>'
         html_to_render.html_safe
     end
 end
