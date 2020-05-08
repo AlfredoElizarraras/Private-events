611a9e4a633a783bbce51a24e1e4b82b1fe98766
flakster <carlos.santamaria@gmail.com>
2020-05-08 11:54:49 -0500

    Add the route get 'users/sign_in' to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  post 'users/logging'
   root 'users#new'
+  get 'users/sign_in'
+  post 'users/logging'
   resources :users, only:  [:show, :new, :create, :sign_in, :logging]
   resources :events, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
