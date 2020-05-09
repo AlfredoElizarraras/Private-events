39a2e520f09fad03bb7cb77eb46d45bf5560b20b
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-09 13:00:56 -0500

    Modify the attendance migration to fix the association between users and events with the alias attendee and attended event.

--- db/migrate/20200508224947_create_attendance.rb
+++ db/migrate/20200508224947_create_attendance.rb
@@ -1,8 +1,8 @@
 class CreateAttendance < ActiveRecord::Migration[6.0]
   def change
     create_table :attendances do |t|
-      t.integer :attendee_id, null: false
-      t.integer :attended_event_id, null: false
+      t.references :attendee, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :attended_event, null: false, index: true, foreign_key: { to_table: :events }
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,10 @@ ActiveRecord::Schema.define(version: 2020_05_08_224947) do
   enable_extension "plpgsql"
 
   create_table "attendances", force: :cascade do |t|
-    t.integer "attendee_id", null: false
-    t.integer "attended_event_id", null: false
+    t.bigint "attendee_id", null: false
+    t.bigint "attended_event_id", null: false
+    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -34,4 +36,6 @@ ActiveRecord::Schema.define(version: 2020_05_08_224947) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "attendances", "events", column: "attended_event_id"
+  add_foreign_key "attendances", "users", column: "attendee_id"
 end
