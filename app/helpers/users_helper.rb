1702ccf630faf4e9374109f69ecff44ef6d80855
flakster <carlos.santamaria@gmail.com>
2020-05-11 17:55:24 -0500

    Add attended events to the users show page and upcomming and past event methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,11 +4,32 @@ module UsersHelper
     end
 
     def user_created_events
-        html_to_render = '<ul>'
-        current_user.events.each do |event|
-            p html_to_render += "<li> #{event.date} </li>"
-        end
-        html_to_render += '</ul>'
+        html_to_render = ''
+        if current_user.events.count > 0
+            html_to_render += "<h3>Events you have created: </h3>"
+            html_to_render += '<ul>'
+            current_user.events.each do |event|
+                html_to_render += "<li> #{event.description} </li>"
+            end
+            html_to_render += '</ul>'
+        else
+            html_to_render += "<h3>You have not created events yet </h3>"
+        end 
+        html_to_render.html_safe
+    end
+
+    def events_attended
+        html_to_render = ''
+        if current_user.attended_events.count > 0
+            html_to_render += "<h3>Events you will attend: </h3>"
+            html_to_render += '<ul>'
+            current_user.attended_events.each do |event|
+                html_to_render += "<li> #{event.description} #{event.date}</li>"
+            end
+            html_to_render += '</ul>'
+        else
+            html_to_render += "<h3>You are not attending events yet </h3>"
+        end 
         html_to_render.html_safe
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :events,  foreign_key: 'creator_id', class_name: 'Event'
   has_many :attendances, foreign_key: :attendee_id
   has_many :attended_events, through: :attendances, class_name: 'Event'
+
+  def upcomming_events
+    attended_events.where('date >= ?', Date.today )
+  end 
+
+  def past_events
+    attended_events.where('date < ?', Date.today )
+  end 
+
 end
