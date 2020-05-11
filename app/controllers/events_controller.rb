4041a984ab39e9adac895c3df612f1509ee1aa6c
flakster <carlos.santamaria@gmail.com>
2020-05-11 16:04:48 -0500

    Add show and index methods for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,14 @@ class EventsController < ApplicationController
   include EventsHelper
   include ApplicationHelper
 
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
     @event = Event.new
   end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,15 @@
 module EventsHelper
     def event_params
-        params.require(:event).permit(:date)
+        params.require(:event).permit(:date, :description )
     end
+
+    def events_list
+        html_to_render = '<ul>'
+        @events.each do |event|
+            html_to_render += "<li> <a href= '/events/#{event.id}'>#{event.description}</a></li>"
+        end
+        html_to_render += '</ul>'
+        html_to_render.html_safe
+    end
+
 end
