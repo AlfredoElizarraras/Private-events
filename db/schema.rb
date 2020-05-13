1efa9eaa9468efb25d7d16b6e4e3f17d7b74142b
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-13 15:07:09 -0500

    Add more fields to Event model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_192242) do
+ActiveRecord::Schema.define(version: 2020_05_13_200132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_05_13_192242) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.string "title", null: false
+    t.string "location"
+    t.boolean "accesibility", default: true, null: false
   end
 
   create_table "invitations", force: :cascade do |t|
