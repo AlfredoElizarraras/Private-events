162f5cfe385de51f189075f36036f24840751eca
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-13 14:39:28 -0500

    Add Invitation model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_192309) do
+ActiveRecord::Schema.define(version: 2020_05_13_192242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 2020_05_12_192309) do
     t.string "description"
   end
 
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "host_id", null: false
+    t.bigint "invitee_id", null: false
+    t.bigint "event_id", null: false
+    t.string "status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_invitations_on_event_id"
+    t.index ["host_id", "invitee_id", "event_id"], name: "index_invitations_on_host_id_and_invitee_id_and_event_id", unique: true
+    t.index ["host_id"], name: "index_invitations_on_host_id"
+    t.index ["invitee_id"], name: "index_invitations_on_invitee_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
@@ -41,4 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_192309) do
 
   add_foreign_key "attendances", "events", column: "attended_event_id"
   add_foreign_key "attendances", "users", column: "attendee_id"
+  add_foreign_key "invitations", "events"
+  add_foreign_key "invitations", "users", column: "host_id"
+  add_foreign_key "invitations", "users", column: "invitee_id"
 end
