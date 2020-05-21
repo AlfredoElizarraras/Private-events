d284faf0e2fa57eb0051a8cc5c8d182866ac2a6c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-05-21 14:58:59 -0500

    Add test for attendances.

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Attendances relation with Users:' do
+    it "can store multiple attendees." do
+      attendee = Attendance.reflect_on_association(:attendee)
+      expect(attendee.macro).to eq(:belongs_to)
+    end
+  end
+
+  context 'Attendances relation with Events:' do
+    it "can store multiple attended events." do
+      attended_event = Attendance.reflect_on_association(:attended_event)
+      expect(attended_event.macro).to eq(:belongs_to)
+    end
+  end
 end
